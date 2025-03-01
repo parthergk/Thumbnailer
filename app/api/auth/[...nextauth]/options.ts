9f70d19c60bb8c50dbdac58dd0fcb76f12ef3c39
parthergk <gauravkumar81464@gmail.com>
2025-03-01 16:59:34 +0530

    ui and login with google

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -57,8 +57,8 @@ export const authOptions: NextAuthOptions = {
     }),
     //google provider 
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret:process.env.GOOGLE_CLIENT_SECRET
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret:process.env.GOOGLE_CLIENT_SECRET as string
     })
   ],
 
