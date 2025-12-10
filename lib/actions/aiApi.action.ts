a6cb0853f8a64dd878d28e2ce13ff224f580a1ae
parthergk <gauravkumar81464@gmail.com>
2025-12-10 21:17:20 +0530

    changed the ai model

--- lib/actions/aiApi.action.ts
+++ lib/actions/aiApi.action.ts
@@ -82,7 +82,7 @@ async function analyzeImage(prompt: string, imageUri: string): Promise<string> {
     }
     
     const genAI = new GoogleGenerativeAI(process.env.GOOGLE_AI_API_KEY);
-    const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+    const model = genAI.getGenerativeModel({ model: 'gemini-2.5-flash' });
 
     // console.log("Prompt", prompt); //log prompt
     
