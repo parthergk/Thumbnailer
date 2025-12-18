a44078cc80bd9d291952d9d5346ff77b376ddd2e
parthergk <gauravkumar81464@gmail.com>
2025-12-18 22:04:44 +0530

    landing page done

--- components/home/FeaturesSection.tsx
+++ components/home/FeaturesSection.tsx
@@ -16,22 +16,22 @@ export function FeaturesSection() {
         <h2 className="text-2xl md:text-4xl font-medium mb-8 text-neutral-900 dark:text-white">
           Powerful Features for Creators
         </h2>
-        <div className=" grid grid-rows-2 md:grid-rows-1 md:grid-cols-[auto_1fr] gap-4 border-t pt-2 ">
+        <div className=" grid grid-rows-2 lg:grid-rows-1 lg:grid-cols-[auto_1fr] gap-4 border-t pt-2 ">
           <div className=" w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 md:grid-rows-3 gap-3 px-2 sm:px-4 py-4 max-w-md border-r">
             {features.map((feature, index) => (
               <FeatureCard key={index} {...feature} />
             ))}
           </div>
-          <div className=" flex flex-col justify-center items-center px-2 sm:px-4 pt-2 gap-4">
+          <div className=" flex flex-col justify-center items-center sm:px-4 pt-2 gap-6 sm:gap-8">
             <div className=" self-end text-xs sm:text-sm  border shadow-md rounded-sm px-4 py-1.5 text-neutral-900 bg-neutral-50 dark:bg-neutral-800 dark:text-white dark:shadow-neutral-950 ">
               How to design Thumbnails with Ai?
             </div>
-            <div className=" w-full h-[500px] max-w-[500px] shadow-lg dark:shadow-neutral-950 bg-white dark:bg-neutral-900 border dark:border-neutral-800 rounded-md">
-              <div className="px-5 w-full h-full flex justify-center items-center">
+            <div className=" h-[400px] w-[320px] sm:w-[500px] shadow-lg dark:shadow-neutral-950 bg-white dark:bg-neutral-900 border dark:border-neutral-800 rounded-md">
+              <div className=" px-3 sm:px-5 w-full h-full flex justify-center items-center">
                 <div className=" relative">
-                  <div className=" absolute border-t border-neutral-500 w-5 left-[286px] top-5"></div>
-                  <div className=" absolute border-r border-neutral-500  h-10 w-px left-[305px] -top-[19px] "></div>
-                  <div className=" absolute bg-white dark:bg-neutral-800 text-neutral-800 dark:text-white text-sm shadow-lg dark:shadow-neutral-950 px-2 py-0.5 rounded-sm left-[229px] -top-11">
+                  <div className=" absolute border-t border-neutral-500 w-5 left-[184px] top-3.5 sm:left-[286px] sm:top-5"></div>
+                  <div className=" absolute border-r border-neutral-500  h-10 w-px left-[204px] -top-[25px] sm:left-[305px] sm:-top-[19px] "></div>
+                  <div className=" absolute bg-white dark:bg-neutral-800 text-neutral-800 dark:text-white text-xs sm:text-sm shadow-lg dark:shadow-neutral-950 px-2 py-0.5 rounded-sm left-[140px] -top-11 sm:left-[229px] sm:-top-11">
                     Font Color: #1E1E1E
                   </div>
                   <Image
@@ -41,17 +41,17 @@ export function FeaturesSection() {
                     src="/image/thumbnail.jpg"
                     className=" object-cover"
                   />
-                  <div className=" absolute bg-white dark:bg-neutral-800 text-neutral-800 dark:text-white text-sm shadow-lg dark:shadow-neutral-950 px-2 py-0.5 rounded-sm -left-14 bottom-28">
+                  <div className=" absolute bg-white dark:bg-neutral-800 text-neutral-800 dark:text-white text-xs sm:text-sm shadow-lg dark:shadow-neutral-950 px-2 py-0.5 rounded-sm -left-8 bottom-[84px] sm:-left-14 sm:bottom-28">
                     Font Size: 24px
                   </div>
-                  <div className=" absolute border-t border-neutral-500 w-5 bottom-[68px] left-1"></div>
-                  <div className=" absolute border-r border-neutral-500  h-11 w-px bottom-[68px] left-1 "></div>
+                  <div className=" absolute border-t border-neutral-500 w-3 sm:w-5 bottom-[40px] left-1 sm:bottom-[68px] sm:left-1"></div>
+                  <div className=" absolute border-r border-neutral-500  h-11 w-px bottom-[40px] left-1 sm:bottom-[68px] sm:left-1 "></div>
                   {/* bg description */}
-                  <div className=" absolute bg-white dark:bg-neutral-800 text-neutral-800 dark:text-white text-sm shadow-lg dark:shadow-neutral-950 px-2 py-0.5 rounded-sm left-40 -bottom-5">
+                  <div className=" absolute bg-white dark:bg-neutral-800 text-neutral-800 dark:text-white text-xs sm:text-sm shadow-lg dark:shadow-neutral-950 px-2 py-0.5 rounded-sm left-[90px] -bottom-[40px] sm:left-40 sm:-bottom-5">
                     Font Size: 24px
                   </div>
-                  <div className=" absolute border-t border-neutral-500 w-7 bottom-[100px] left-52"></div>
-                  <div className=" absolute border-r border-neutral-500  h-24 w-px bottom-1 left-52 "></div>
+                  <div className=" absolute border-t border-neutral-500 w-4 sm:w-7 bottom-[70px] left-[140px] sm:bottom-[100px] sm:left-52"></div>
+                  <div className=" absolute border-r border-neutral-500  h-24 w-px -bottom-[25px] left-[140px] sm:bottom-1 sm:left-52 "></div>
                 </div>
               </div>
             </div>
