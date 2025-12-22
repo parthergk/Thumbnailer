c3265e575e3f1bbd7982c7d5475854d5f877a0f5
parthergk <gauravkumar81464@gmail.com>
2025-12-22 17:23:50 +0530

    fix the color and color clip

--- components/resourcesItem/Color.tsx
+++ components/resourcesItem/Color.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import { Pipette } from 'lucide-react';
-import ColorClip from './ColorClip';
-import Field from './Field';
+import React, { useState } from "react";
+import { Pipette } from "lucide-react";
+import ColorClip from "./ColorClip";
+import Field from "./Field";
 
 // Define the structure of a single color
 interface ColorProps {
@@ -15,12 +15,12 @@ interface ColorComponentProps {
 }
 
 const Color: React.FC<ColorComponentProps> = ({ data }) => {
-  const [eyeColor, setEyeColor] = useState<string>(''); // State for storing the picked color
+  const [eyeColor, setEyeColor] = useState<string>(""); // State for storing the picked color
 
   // Function to open the EyeDropper API and pick a color
   const eyeDropper = async () => {
-    if (!('EyeDropper' in window)) {
-      alert('EyeDropper API is not supported in this browser.');
+    if (!("EyeDropper" in window)) {
+      alert("EyeDropper API is not supported in this browser.");
       return;
     }
 
@@ -38,15 +38,17 @@ const Color: React.FC<ColorComponentProps> = ({ data }) => {
     <>
       <div className="w-full flex flex-col gap-4 my-3 ">
         {data.map((color, index) => (
-          <div key={(color.code,index)} className="flex flex-col">
-            <Field label={color.name} value={color.code} copy/>
+          <div key={(color.code, index)} className="flex flex-col">
+             <ColorClip label={color.name} color={color.code}/>
           </div>
         ))}
       </div>
 
       <div className="mt-5">
         <div className="flex justify-between">
-          <label className="text-sm text-neutral-700 dark:text-neutral-300">Custom Color</label>
+          <label className="text-sm text-neutral-700 dark:text-neutral-300">
+            Custom Color
+          </label>
           <button onClick={eyeDropper}>
             <Pipette className="h-4 w-4 text-neutral-700 dark:text-white" />
           </button>
--- components/resourcesItem/ColorClip.tsx
+++ components/resourcesItem/ColorClip.tsx
@@ -1,24 +1,30 @@
-import CopyClip from './CopyClip';
+import CopyClip from "./CopyClip";
 
 interface ColorClipProps {
-    color: string;
+  color: string;
+  label?: string;
 }
 
-const ColorClip: React.FC<ColorClipProps> = ({ color }) => {
-    return (
-        <div className="flex gap-2 mt-1">
-            <div
-                className="w-10 h-10 border"
-                style={{ backgroundColor: color || '#ffffff' }}
-            />
-            <input
-                value={color || ''}
-                readOnly
-                className="flex-1 text-center text-neutral-800 dark:text-neutral-300 border rounded-sm w-full outline-none shadow-lg"
-            />
-            <CopyClip copyClip={color} />
-        </div>
-    );
+const ColorClip: React.FC<ColorClipProps> = ({ label, color }) => {
+  return (
+    <div className="flex flex-col gap-1">
+      <span className="text-sm text-neutral-700 dark:text-neutral-300">
+        {label}
+      </span>
+      <div className="flex gap-2 mt-1">
+        <div
+          className="w-10 h-10 border"
+          style={{ backgroundColor: color || "#ffffff" }}
+        />
+        <input
+          value={color || ""}
+          readOnly
+          className="flex-1 text-center text-neutral-800 dark:text-neutral-300 border rounded-sm w-full outline-none shadow-lg"
+        />
+        <CopyClip copyClip={color} />
+      </div>
+    </div>
+  );
 };
 
 export default ColorClip;
